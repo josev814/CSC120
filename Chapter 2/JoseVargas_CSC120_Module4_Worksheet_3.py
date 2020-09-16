29accfa60130ccdad86ee3d6d791799f3c7296f7
josev814 <josev814@gmail.com>
2020-09-16 12:26:17 -0400

    adding worksheet 3

---
+++ Chapter 2/JoseVargas_CSC120_Module4_Worksheet_3.py
@@ -0,0 +1,22 @@
+print("""Write the following mathematical expressions in Python. Assume, x, y, z, a, b, c are variables. 
+xy(z+a) + b3 
+(x+y)(a+b+2)5 
+X2y + ab3 """)
+
+x, y, z, a, b, c = 11, 12, 13, 1, 2, 3
+print(
+    'Python code:',
+    'print(x*y*(z+a)+b**3, x+y*((a+b+2)**5), (x**2)*y+a*(b**3))'
+)
+print('Answer:', x*y*(z+a)+b**3, x+y*((a+b+2)**5), (x**2)*y+a*(b**3))
+print()
+print("""Write a program that will prompt the user to enter the size of square with an inscribed circle. The program will calculate the area of the shaded region using the formula:  
+
+area of shaded region = area of square – area of circle = s2 (1- p/4), p = 3.1414 """)
+
+square_length = float(input('What is the length of the square? '))
+area_of_square = square_length**2
+area_of_circle = area_of_square*(1.0 - 3.1313/4)
+area_of_shaded_region = area_of_square - area_of_circle
+print('The shaded area of the square is {} '.format(area_of_shaded_region))
+
