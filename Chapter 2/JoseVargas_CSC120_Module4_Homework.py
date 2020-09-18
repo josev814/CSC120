64e31bd9041dc546b5812c950670364c68ed504c
josev814 <josev814@gmail.com>
2020-09-18 12:47:27 -0400

    adding homework program for Module 4 Homework 1

---
+++ Chapter 2/JoseVargas_CSC120_Module4_Homework.py
@@ -0,0 +1,4 @@
+print('-'*50, '-'*50, '-'*50)
+print("Write Python code that prompts the user to enter his or her favorite color and assigns the user’s input to a variable named color ")
+color = input('What is your favorite color? ')
+print('Your favorite color is ', color)
